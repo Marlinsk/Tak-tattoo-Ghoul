a3d39a8690ccb510fd2457b827e4ab66cda35d7b
Marlon Rodrigues <Marlonnlmx@outlook.com>
2021-05-18 17:52:02 -0300

    Modifications

--- tak-next/pages/index.js
+++ tak-next/pages/index.js
@@ -2,9 +2,28 @@ import Head from 'next/head';
 
 export default function Home() {
 
+    var imageList = [
+        "/images/image-tattoo/category/egyptian/7712971313e244c0f0672824d3acc182.jpg",
+        "/images/image-tattoo/category/angels/980acb3243ec7b1d61d7c967c9649432.jpg",
+        "/images/image-tattoo/category/angels/36bde2d8e4ccb9e4d5477f6c4f39b3ff.jpg",
+        "/images/image-tattoo/category/egyptian/b96bc38958a94283fed7bf8b01e81078.jpg",
+        "/images/image-tattoo/category/angels/51e0e04943d40670872d7d3a6cba5a44.jpg",
+        "/images/image-tattoo/category/gothic/d8842b380d2d6652e3d01516bdd503fd.jpg",
+        "/images/image-tattoo/category/animal/170080334ee740aee275cecd356020c0.jpg",
+        "/images/image-tattoo/category/geometric/61977c2507788c80d6d0a355894eef5f.jpg",
+        "/images/image-tattoo/category/eyes/2294346cf8823f9ee5c7346012a6befb.jpg",
+        "/images/image-tattoo/category/eyes/7e72b0c8e35e3aea4f924926a28f4b3b.jpg",
+        "/images/image-tattoo/category/geometric/e95e5a0f2bcaf4d9759a5d544741dbf7.jpg",
+        "/images/image-tattoo/category/science/5d0020f0571527960babe6a6ed9a8c02.jpg",
+        "/images/image-tattoo/category/surrealist/35ab76c681bcc9a56f9d0d87148b9889.jpg",
+        "/images/image-tattoo/category/angels/5281177fab08489472d1c5eeeb51c540.jpg",
+        "/images/image-tattoo/category/geometric/eb799888e1b722e3b3c0ccdce8374fdd.jpg",        
+    ]
+
     return (
         <>
             <Head>
+                <link rel="icon" href="/images/logo/Icone branco sem fundo.png" />
                 <title>Tak Ghoul</title>
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Neue&family=Montserrat:wght@500&display=swap" rel="stylesheet" />
@@ -22,6 +41,7 @@ export default function Home() {
                     <a className="conect">
                         <p>Conectar</p>
                     </a>
+                    <input type="text"/>
                 </nav>
             </header>
 
@@ -78,66 +98,13 @@ export default function Home() {
 
                 <div>
                     <section className="layout-grid">
-
-                        <div className="img-content">
-                            <img src="/images/image-tattoo/category/egyptian/7712971313e244c0f0672824d3acc182.jpg" />
-                        </div>
-
-                        <div className="img-content">
-                            <img src="/images/image-tattoo/category/angels/980acb3243ec7b1d61d7c967c9649432.jpg" />
-                        </div>
-
-                        <div className="img-content">
-                            <img src="/images/image-tattoo/category/angels/36bde2d8e4ccb9e4d5477f6c4f39b3ff.jpg" />
-                        </div>
-
-                        <div className="img-content">
-                            <img src="/images/image-tattoo/category/egyptian/b96bc38958a94283fed7bf8b01e81078.jpg" />
-                        </div>
-
-                        <div className="img-content">
-                            <img src="/images/image-tattoo/category/angels/51e0e04943d40670872d7d3a6cba5a44.jpg" />
-                        </div>
-
-                        <div className="img-content">
-                            <img src="/images/image-tattoo/category/gothic/d8842b380d2d6652e3d01516bdd503fd.jpg" />
-                        </div>
-
-                        <div className="img-content">
-                            <img src="/images/image-tattoo/category/animal/170080334ee740aee275cecd356020c0.jpg" />
-                        </div>
-
-                        <div className="img-content">
-                            <img src="/images/image-tattoo/category/geometric/61977c2507788c80d6d0a355894eef5f.jpg" />
-                        </div>
-
-                        <div className="img-content">
-                            <img src="/images/image-tattoo/category/eyes/2294346cf8823f9ee5c7346012a6befb.jpg" />
-                        </div>
-
-                        <div className="img-content">
-                            <img src="/images/image-tattoo/category/eyes/7e72b0c8e35e3aea4f924926a28f4b3b.jpg" />
-                        </div>
-
-                        <div className="img-content">
-                            <img src="/images/image-tattoo/category/geometric/e95e5a0f2bcaf4d9759a5d544741dbf7.jpg" />
-                        </div>
-
-                        <div className="img-content">
-                            <img src="/images/image-tattoo/category/science/5d0020f0571527960babe6a6ed9a8c02.jpg" />
-                        </div>
-
-                        <div className="img-content">
-                            <img src="/images/image-tattoo/category/surrealist/35ab76c681bcc9a56f9d0d87148b9889.jpg" />
-                        </div>
-
-                        <div className="img-content">
-                            <img src="/images/image-tattoo/category/angels/5281177fab08489472d1c5eeeb51c540.jpg" />
-                        </div>
-                        <div className="img-content">
-                            <img src="/images/image-tattoo/category/geometric/eb799888e1b722e3b3c0ccdce8374fdd.jpg" />
-                        </div>
-
+                        {imageList.map((item, key) => {
+                            return (    
+                                <div className="img-content" key={key}>
+                                    <img src={item} />
+                                </div>
+                            )
+                        })}
                     </section>
                 </div>
             </div>
