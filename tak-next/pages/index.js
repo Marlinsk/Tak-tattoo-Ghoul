1260b299063b33b186cf6000fdeb1b04b0c5ce6c
Marlon Rodrigues <Marlonnlmx@outlook.com>
2021-05-23 19:18:24 -0300

    Update to search-bar

--- tak-next/pages/index.js
+++ tak-next/pages/index.js
@@ -1,28 +1,32 @@
 import Head from 'next/head';
 import React, { useRef } from 'react';
 
-/*
-var containexBox = document.querySelector('.container-box');
-
-containexBox.addEventListener("wheel", function (e) {
-    if (e.deltaY > 0) containexBox.scrollLeft += 400;
-    else containexBox.scrollLeft -= 400;
-});*/
-
 export default function Home() {
 
     var containerBox = useRef(null);
+    var dropDown = useRef(null);
+    var rotateArrowDropDown = useRef(null);
 
     function rolagem(e) {
         if (e.deltaY > 0) {
             containerBox.current.scrollLeft += 400;
         }
         else {
+            e.preventDefault();
             containerBox.current.scrollLeft -= 400;
-            console.log(containerBox);
         }
     }
-    
+
+    function openDropDown() {
+        var a = dropDown.current.classList.toggle('close');
+        var rotate = 0;
+        if (a !== true) {
+            rotate = -180;
+        }
+        rotateArrowDropDown.current.style.transform = `rotate(${rotate}deg)`;
+
+    }
+
     var imageList = [
         "/images/image-tattoo/category/egyptian/7712971313e244c0f0672824d3acc182.jpg",
         "/images/image-tattoo/category/angels/980acb3243ec7b1d61d7c967c9649432.jpg",
@@ -46,14 +50,15 @@ export default function Home() {
             <Head>
                 <link rel="icon" href="/images/logo/Icone branco sem fundo.png" />
                 <title>Tak Ghoul</title>
-                
+
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Neue&family=Montserrat:wght@500&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=Pattaya&display=swap" rel="stylesheet" />
-                <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet"/>
+                <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&family=Mandali&display=swap" rel="stylesheet"></link>
 
                 <link rel="stylesheet" href="/styles/navbar.css" type="text/css" media="all" />
                 <link rel="stylesheet" href="/styles/grid.css" type="text/css" media="all" />
@@ -73,11 +78,19 @@ export default function Home() {
                                 <a className="btn-location" href="">
                                     <img src="/icon/cursor.svg" alt="location-icon" />
                                 </a>
-                                <a className="btn-down-list" href="">
-                                    <img src="/icon/arrow-down-sign-to-navigate.svg" alt="dopdown-arrow-btn" />
-                                </a>
+                                <p className="btn-down-list" onClick={() => openDropDown()}>
+                                    <img src="/icon/arrow-down-sign-to-navigate.svg" alt="dopdown-arrow-btn" ref={rotateArrowDropDown} />
+                                </p>
+                                <ul className="dropdown close" ref={dropDown}>
+                                    <li className="itemdropdown">Lista</li>
+                                    <li className="itemdropdown">Listax</li>
+                                    <li className="itemdropdown">Listac</li>
+                                    <li className="itemdropdown">Listaa</li>
+                                    <li className="itemdropdown">Lista</li>
+                                    <li className="itemdropdown">Listd</li>
+                                </ul>
                             </div>
-                        </div>                 
+                        </div>
                         <ul>
                             <li><a>Início</a></li>
                             <li><a>Estilos</a></li>
@@ -86,9 +99,9 @@ export default function Home() {
                         </ul>
                     </nav>
                 </header>
-                
+
                 <br /><br />
-                
+
                 <h3 className="subtitle1"> Estúdios populares em . . . </h3>
                 <div class="container-box" ref={containerBox} onWheel={(e) => rolagem(e)}>
                     <div class="boxes">
@@ -110,9 +123,9 @@ export default function Home() {
                         <div class="box"></div>
                     </div>
                 </div>
-                
+
                 <br /><br />
-                
+
                 <h3 className="subtitle1"> Postagens de tattoo recentes em . . . </h3>
                 <div>
                     <section className="layout-grid">
@@ -121,7 +134,7 @@ export default function Home() {
                                 <div className="img-content" key={key}>
                                     <img src={item} />
                                     <div className="Feed">
-                                        <foto/>
+                                        <foto />
                                         <p href="">Fulano Studio</p>
                                     </div>
                                 </div>
@@ -129,7 +142,7 @@ export default function Home() {
                         })}
                     </section>
                 </div>
-                 
+
                 <div id="modal-conectar" className="modal-container">
                     <div className="modal">
                         <button className="close-login">x</button>
