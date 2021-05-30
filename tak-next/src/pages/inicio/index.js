9add861d1642fee616442bcaeadd03d76b163b52
Marlon Rodrigues <Marlonnlmx@outlook.com>
2021-05-29 21:34:56 -0300

    Update to page 'inicial'

--- tak-next/src/pages/inicio/index.js
+++ tak-next/src/pages/inicio/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import React, { useRef } from 'react';
+import { feedList } from './places/tattoo-places-v.js';
 
 export default function Home() {
 
@@ -26,24 +27,6 @@ export default function Home() {
         rotateArrowDropDown.current.style.transform = `rotate(${rotate}deg)`;
 
     }
-
-    var imageList = [
-        "/images/image-tattoo/category/egyptian/7712971313e244c0f0672824d3acc182.jpg",
-        "/images/image-tattoo/category/angels/980acb3243ec7b1d61d7c967c9649432.jpg",
-        "/images/image-tattoo/category/angels/36bde2d8e4ccb9e4d5477f6c4f39b3ff.jpg",
-        "/images/image-tattoo/category/egyptian/b96bc38958a94283fed7bf8b01e81078.jpg",
-        "/images/image-tattoo/category/angels/51e0e04943d40670872d7d3a6cba5a44.jpg",
-        "/images/image-tattoo/category/gothic/d8842b380d2d6652e3d01516bdd503fd.jpg",
-        "/images/image-tattoo/category/animal/170080334ee740aee275cecd356020c0.jpg",
-        "/images/image-tattoo/category/geometric/61977c2507788c80d6d0a355894eef5f.jpg",
-        "/images/image-tattoo/category/eyes/2294346cf8823f9ee5c7346012a6befb.jpg",
-        "/images/image-tattoo/category/eyes/7e72b0c8e35e3aea4f924926a28f4b3b.jpg",
-        "/images/image-tattoo/category/geometric/e95e5a0f2bcaf4d9759a5d544741dbf7.jpg",
-        "/images/image-tattoo/category/science/5d0020f0571527960babe6a6ed9a8c02.jpg",
-        "/images/image-tattoo/category/surrealist/35ab76c681bcc9a56f9d0d87148b9889.jpg",
-        "/images/image-tattoo/category/angels/5281177fab08489472d1c5eeeb51c540.jpg",
-        "/images/image-tattoo/category/geometric/eb799888e1b722e3b3c0ccdce8374fdd.jpg",
-    ];
 
     return (
         <>
@@ -119,7 +102,7 @@ export default function Home() {
                 <h3 className="subtitle1"> Postagens de tattoo recentes em . . . </h3>
                 <div>
                     <section className="layout-grid">
-                        {imageList.map((item, key) => {
+                        {feedList.map((item, key) => {
                             return (
                                 <div className="img-content" key={key}>
                                     <img id="i-tattoo" className="img-tattoo" src={item} />
--- tak-next/src/pages/inicio/places/tattoo-places-v.js
+++ tak-next/src/pages/inicio/places/tattoo-places-v.js
@@ -1,4 +1,4 @@
-var imageList = [
+export var feedList = [
     "/images/image-tattoo/category/egyptian/7712971313e244c0f0672824d3acc182.jpg",
     "/images/image-tattoo/category/angels/980acb3243ec7b1d61d7c967c9649432.jpg",
     "/images/image-tattoo/category/angels/36bde2d8e4ccb9e4d5477f6c4f39b3ff.jpg",
@@ -13,5 +13,5 @@ var imageList = [
     "/images/image-tattoo/category/science/5d0020f0571527960babe6a6ed9a8c02.jpg",
     "/images/image-tattoo/category/surrealist/35ab76c681bcc9a56f9d0d87148b9889.jpg",
     "/images/image-tattoo/category/angels/5281177fab08489472d1c5eeeb51c540.jpg",
-    "/images/image-tattoo/category/geometric/eb799888e1b722e3b3c0ccdce8374fdd.jpg",
+    "/images/image-tattoo/category/geometric/eb799888e1b722e3b3c0ccdce8374fdd.jpg"
 ];
