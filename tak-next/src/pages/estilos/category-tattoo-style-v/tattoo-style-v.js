505356c95728ec3e070aae9c89de6e3e9126cbf1
Marlon Rodrigues <Marlonnlmx@outlook.com>
2021-06-01 01:29:08 -0300

    Update to tattoo-style-v.js

--- tak-next/src/pages/estilos/category-tattoo-style-v/tattoo-style-v.js
+++ tak-next/src/pages/estilos/category-tattoo-style-v/tattoo-style-v.js
@@ -1,3 +1,18 @@
+    export var categorytattoo = [
+        "images/image-tattoo/category/angels/0dd06aa4c69e1b77077a9d2a4a52a7fe.jpg",
+        "images/image-tattoo/category/animal/4e5cccf927cbb6ab7c1d3e0f254ada76.jpg",
+        "images/image-tattoo/category/science/e0869f1f6069f2ddd5b248dbc322cb39.jpg",
+        "images/image-tattoo/category/egyptian/5d4ead5da755284a723904db2df08ec8.jpg",
+        "images/image-tattoo/category/space/6ec4a9b2eae625c27eecf7e4695e4cfa.jpg",
+        "images/image-tattoo/category/sci-fi/6df9a4173cb30b0772f63d910074ac8e.jpg",
+        "images/image-tattoo/category/flowers/53044109853be52e38784ae0a6a78f79.jpg",
+        "images/image-tattoo/category/geometric/14f4c8cb38c4ac62d8e826a5ade8ff8a.jpg",
+        "images/image-tattoo/category/gothic/b4a1605f75c172ac6293df243695c634.jpg",
+        "images/image-tattoo/category/oriental/48bee0bcd92bd82e81cdbff091a721fc.jpg",
+        "images/image-tattoo/category/surrealist/0e8722d8c2bcc772f9f921a298077b28.jpg",
+        "images/image-tattoo/category/eyes/2294346cf8823f9ee5c7346012a6befb.jpg"
+    ];
+
     export var categoryAngels = [
         "images/image-tattoo/category/angels/36bde2d8e4ccb9e4d5477f6c4f39b3ff.jpg",
         "images/image-tattoo/category/angels/51e0e04943d40670872d7d3a6cba5a44.jpg",
